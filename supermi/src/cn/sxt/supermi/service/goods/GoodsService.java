ad521d346adce5dc618d1bce361f4f6d8ef5370b
bo1214 <790024786@qq.com>
2018-06-11 16:21:30 +0800

    zhuyunbo

--- supermi/src/cn/sxt/supermi/service/goods/GoodsService.java
+++ supermi/src/cn/sxt/supermi/service/goods/GoodsService.java
@@ -1,5 +1,46 @@
 package cn.sxt.supermi.service.goods;
 
-public interface GoodsService {
+import java.util.List;
 
+import cn.sxt.supermi.entity.Goods;
+
+public interface GoodsService {
+	/**
+	 * 查询所有的商品信息
+	 * @return   商品List集合
+	 */
+	public List<Goods> getAll();
+	/**
+	 * 通过商品id查询商品信息
+	 * @return   商品List集合
+	 */
+	public List<Goods> getAllByID(Goods gid);
+	/**
+	 *通过商品名查询商品信息
+	 * @param gName
+	 * @return
+	 */
+	public List<Goods> getAllByName(String gName);
+	/**
+	 *增加商品
+	 * @return   int
+	 */
+	public int addGoods(Goods g);
+	/**
+	 *删除商品
+	 * @return   int
+	 */
+	public int deleteGoods(int gid);
+	/**
+	 *修改商品
+	 * @return   int
+	 */
+	public int updateGoods(Goods g);
+	/**
+	 * 更新商品销量
+	 * @param gid 商品id
+	 * @param num 要添加的商品销量(销量要增加的数量)
+	 * @return 影响的行数
+	 */
+	public int updateGoodsSalesByID(int gid,int num);
 }
--- supermi/src/cn/sxt/supermi/service/goods/impl/GoodsServiceImpl.java
+++ supermi/src/cn/sxt/supermi/service/goods/impl/GoodsServiceImpl.java
@@ -1,7 +1,57 @@
 package cn.sxt.supermi.service.goods.impl;
 
+import java.util.List;
+
+import cn.sxt.supermi.dao.goods.GoodsDAO;
+import cn.sxt.supermi.dao.goods.impl.GoodsDAOImpl;
+import cn.sxt.supermi.entity.Goods;
 import cn.sxt.supermi.service.goods.GoodsService;
 
 public class GoodsServiceImpl implements GoodsService{
 
+	GoodsDAO gdao = new GoodsDAOImpl();
+	
+	public List<Goods> getAll() {
+		
+		return gdao.getAll();
+	}
+
+	
+	public List<Goods> getAllByID(Goods gid) {
+		// TODO Auto-generated method stub
+		return gdao.getAllByID(gid);
+	}
+
+	
+	public int addGoods(Goods g) {
+		// TODO Auto-generated method stub
+		return gdao.addGoods(g);
+	}
+
+	
+	public int deleteGoods(int gid) {
+		// TODO Auto-generated method stub
+		return gdao.deleteGoods(gid);
+	}
+
+	
+	public int updateGoods(Goods g) {
+		// TODO Auto-generated method stub
+		return gdao.updateGoods(g);
+	}
+
+
+	@Override
+	public int updateGoodsSalesByID(int gid, int num) {
+		
+		return gdao.updateGoodsSalesByID(gid, num);
+	}
+
+
+	@Override
+	public List<Goods> getAllByName(String gName) {
+		
+		return gdao.getAllByName(gName);
+	}
+			
 }
